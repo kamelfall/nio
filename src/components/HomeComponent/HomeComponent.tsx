9cd8f7001e01dc073d00236474e6b80bf91b661a
Erik Målsäter <erik.malsater92@gmail.com>
2019-08-27 11:02:40 +0200

    adds menuInformation and restaurantMenu and basic styling

--- src/components/HomeComponent/HomeComponent.tsx
+++ src/components/HomeComponent/HomeComponent.tsx
@@ -10,6 +10,19 @@ class HomeComponent extends React.Component<{}, {}> {
           <p>구</p>
           <h1>nio</h1>
         </section>
+        <section className="menuInformation">
+          <p>Konceptet är duvet någonting väldigt komplicerat</p>
+        </section>
+        <section className="restaurantMenu">
+          <h2>Bao's</h2>
+          <ul>
+            <li>Kimchi</li>
+            <li>Shiitake</li>
+            <li>Fläsk</li>
+            <li>Kyckling</li>
+            <li>Tofu</li>
+          </ul>
+        </section>
       </main>
     );
   }
