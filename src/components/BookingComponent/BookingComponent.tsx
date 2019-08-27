f162835a80644b3c2d251dc4f28550c099eab872
Lukas Lennstring <lukas.lennstring@gmail.com>
2019-08-27 10:43:07 +0200

    Adds empty booking component

---
+++ src/components/BookingComponent/BookingComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface IBookingProps {
+
+}
+
+export class BookingComponent extends React.Component<IBookingProps,{}> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main>
+
+      </main>
+    );
+  }
+}
