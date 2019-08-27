4dd7f7738163cdb02b64d7633665ed30c6e0b9cf
Erik Målsäter <erik.malsater92@gmail.com>
2019-08-27 10:19:21 +0200

    makes HomeComponent and adds hero image to it

---
+++ src/components/HomeComponent/HomeComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeComponent.css';
+
+class HomeComponent extends React.Component<{}, {}> {
+  
+  render() {
+    return(
+      <main>
+        <section className="heroImage">
+
+        </section>
+      </main>
+    );
+  }
+}
+export default HomeComponent;
