8efc74a2a6161b70341608d83c34cae8954e240d
Lukas Lennstring <lukas.lennstring@gmail.com>
2019-08-27 10:51:23 +0200

    Adds basic select and title

--- src/components/BookingComponent/BookingComponent.tsx
+++ src/components/BookingComponent/BookingComponent.tsx
@@ -12,6 +12,16 @@ export class BookingComponent extends React.Component<IBookingProps,{}> {
   render() {
     return (
       <main>
+        <h1>Boka</h1>
+        <select id="guestSelect">
+          <option value="">Antal gäster</option>
+          <option value="1">1 person</option>
+          <option value="2">2 personer</option>
+          <option value="3">3 personer</option>
+          <option value="4">4 personer</option>
+          <option value="5">5 personer</option>
+          <option value="6">6 personer</option>
+        </select>
 
       </main>
     );
