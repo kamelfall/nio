673d973da22b8fccfae8a9bfaa7a5e7db30c1783
Erik Målsäter <erik.malsater92@gmail.com>
2019-08-28 14:20:33 +0200

    adds routing to App.tsx for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-
 import './App.scss';
 
 import HomeComponent from './components/HomeComponent/HomeComponent';
 import NavbarComponent from './components/NavbarComponent/NavbarComponent';
 import BookingComponent from './components/BookingComponent/BookingComponent';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 class App extends React.Component<{},{}> {
   render() {
     return (
-      <div className="App">
-        <NavbarComponent/>
-        <HomeComponent />
-        <BookingComponent />
-      </div>
+      <Router>
+        <div className="App">
+          <NavbarComponent/>
+          <Switch>
+            <Route path="/" exact component={HomeComponent} />
+            <Route path="/booking" component={BookingComponent} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
