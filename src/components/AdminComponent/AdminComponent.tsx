e075a11e081b740fbe87995915f50e1448e54b41
Olivia <olivia@mail.com>
2019-09-06 15:31:40 +0200

    render bookings on admin page

--- src/components/AdminComponent/AdminComponent.tsx
+++ src/components/AdminComponent/AdminComponent.tsx
@@ -1,9 +1,59 @@
 import * as React from "react";
 import "./AdminComponent.scss";
+import axios from 'axios';
 
-class AdminComponent extends React.Component<{}, {}> {
-  
+interface IBooking {
+  customer_id: number,
+  date: string,
+  email: string,
+  first_name: string,
+  last_name: string,
+  order_id: number,
+  phone: string,
+  seats: number,
+  time: string
+}
+
+interface IBookingState {
+  bookings: IBooking[]
+}
+
+class AdminComponent extends React.Component<{}, IBookingState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      bookings: []
+    }
+  }
+  componentDidMount() {
+    axios.get("http://localhost:8888/order/readAll.php")
+      .then((result: any) => {
+        let array = this.state.bookings;
+        let data:[] = result.data.records;
+        for(let i = 0; i < data.length; i++) {
+          array.push(data[i]);
+        }
+        this.setState({bookings: array});
+      })
+      console.log(this.state.bookings);
+      
+    }
+
   public render() {
+
+    const orders = this.state.bookings;
+    const mappedOrders = orders.map(order => 
+      <tr>
+      <td>{order.order_id}</td>
+      <td>{order.date}</td>
+      <td>{order.time}</td>
+      <td>{order.first_name} {order.last_name}</td>
+      <td>{order.seats}</td>
+      <td>{order.email}</td>
+      </tr>)
+    
+
     return (
     <main>
       <section>
@@ -12,14 +62,20 @@ class AdminComponent extends React.Component<{}, {}> {
       </section>
       <article>
         <table>
-          <tr>
-            <th>Bookingnr</th>
-            <th>Date</th>
-            <th>Sitting</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-          <tr></tr>
+          <thead>
+            <tr>
+              <th>Bookingnr</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Name</th>
+              <th>Seats</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+           {mappedOrders}
+          </tbody>
         </table>
       </article>
 
